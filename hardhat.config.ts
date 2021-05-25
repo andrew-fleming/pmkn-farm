ff1ce18840fab9faf5a3501a9567e884c262d8d6
Andrew <fleming-andrew@protonmail.com>
2021-05-25 05:55:30 -0400

    Added optimizer and dotenv config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,8 @@ import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-web3";
 
+require('dotenv').config()
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (args, hre) => {
@@ -16,5 +18,21 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 export default {
-  solidity: "0.8.4",
-};
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    kovan: {
+        gas: "auto",
+        gasPrice: "auto",
+        url: process.env.API_KEY,
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  }
+}
