f29ab0411789631c7619f6410623e4eb605fd3b6
Andrew <fleming-andrew@protonmail.com>
2021-06-09 23:26:27 -0400

    Added access control for pmknToken

--- contracts/PmknToken.sol
+++ contracts/PmknToken.sol
@@ -2,17 +2,17 @@
 pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract PmknToken is ERC20, Ownable {
+contract PmknToken is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor() ERC20("PmknToken", "PMKN") {}
-
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    constructor() ERC20("PmknToken", "PMKN") {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
-    
-    function _transferOwnership(address newOwner) public onlyOwner {
-        transferOwnership(newOwner);
+
+    function mint(address to, uint256 amount) public {
+        require(hasRole(MINTER_ROLE, msg.sender));
+        _mint(to, amount);
     }
 }
