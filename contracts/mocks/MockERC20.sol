cac9495cf6e62cd58e5083e17ad1f5e801387daa
Andrew <fleming-andrew@protonmail.com>
2021-06-18 21:28:30 -0400

    Updated farm test

---
+++ contracts/mocks/MockERC20.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MockERC20 is ERC20 {
+
+    constructor(
+        string memory name,
+        string memory ticker
+    ) ERC20(name, ticker) {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
