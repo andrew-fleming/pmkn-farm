1ca168bed5668e2f1340f02c2981977c7763a04e
Andrew <fleming-andrew@protonmail.com>
2021-06-20 01:39:37 -0400

    Finished lottery test with MockLotteryFuncs

--- test/Lottery.test.ts
+++ test/Lottery.test.ts
@@ -1,10 +1,8 @@
-import { ethers, waffle } from "hardhat";
-import chai, { expect, use} from "chai";
+import { ethers } from "hardhat";
+import { expect } from "chai";
 import { Contract } from "ethers";
-import { solidity, deployMockContract, MockProvider } from "ethereum-waffle";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
-use(solidity);
 
 describe("Lottery Contract", () => {
 
@@ -14,20 +12,28 @@ describe("Lottery Contract", () => {
 
     let lottery: Contract;
     let jackContract: Contract;
+    let pmknToken: Contract;
+    let mockLink: Contract;
 
-    before(async() => {
-        const Lottery = await ethers.getContractFactory("Lottery");
+    beforeEach(async() => {
+        const Lottery = await ethers.getContractFactory("MockLotteryFunctions");
         const JackContract = await ethers.getContractFactory("JackOLantern");
-
+        const PmknToken = await ethers.getContractFactory("PmknToken");
+        const MockLink = await ethers.getContractFactory("MockERC20");
+        mockLink = await MockLink.deploy("MockLink", "mLINK");
+        pmknToken = await PmknToken.deploy();
         [owner, alice, bob] = await ethers.getSigners();
 
+        await mockLink.mint(owner.address, ethers.utils.parseEther("9999"));
+
         let lotteryParams = [
+            pmknToken.address,
+            mockLink.address,
             "0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9",
             "0xa36085F69e2889c224210F603D836748e7dC0088",
             1, 
             "0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4"
         ];
-
         jackContract = await JackContract.deploy();
         lottery =  await Lottery.deploy(jackContract.address, ...lotteryParams);
 
@@ -38,63 +44,100 @@ describe("Lottery Contract", () => {
             expect(lottery).to.be.ok
             expect(jackContract).to.be.ok
         })
-
         it("should track tokenIds", async() => {
             let minter = await jackContract.MINTER_ROLE()
             await jackContract.grantRole(minter, owner.address)
             await jackContract.safeMint(alice.address)
             await jackContract.safeMint(alice.address)
-
             let res = await jackContract.getTotalSupply()
-
             expect(await jackContract.getTotalSupply())
                 .to.eq(2)
         })
+    })
 
-        it("should ")
+    describe("Link interaction", async() => {
+        it("should accept link", async() => {
+            let res = await mockLink.balanceOf(lottery.address)
+            expect(res).to.eq(0)
+            await mockLink.transfer(lottery.address, 1)
+            expect(await mockLink.balanceOf(lottery.address))
+                .to.eq("1")
+        })
+
+        it("should return correct balance", async() => {
+            await mockLink.transfer(lottery.address, ethers.utils.parseEther("10"))
+            
+            let res = await lottery.getLinkBalance()
+            expect(res)
+                .to.eq(ethers.utils.parseEther("10"))
+        })
+
+        it("should withdraw link", async() => {
+            await mockLink.transfer(lottery.address, ethers.utils.parseEther("10"))
+            await lottery.withdrawLink()
+            expect(await mockLink.balanceOf(lottery.address))
+                .to.eq("0")
+            expect(await mockLink.balanceOf(owner.address))
+                .to.eq(ethers.utils.parseEther("9999"))
+        })
+
+        it("should revert non owner withdraw call", async() => {
+            await mockLink.transfer(lottery.address, ethers.utils.parseEther("10"))
+            await expect(lottery.connect(alice).withdrawLink())
+                .to.be.revertedWith("caller is not the owner")
+        })
 
     })
+
+    describe("Call Test functions to check state", async() => {
+        beforeEach(async() => {
+            let minter = await jackContract.MINTER_ROLE()
+            await Promise.all([
+                jackContract.grantRole(minter, owner.address),
+                pmknToken.grantRole(minter, owner.address),
+                pmknToken.mint(owner.address, ethers.utils.parseEther("999")),
+                jackContract.safeMint(alice.address),
+                jackContract.safeMint(bob.address),
+                jackContract.safeMint(alice.address),
+                jackContract.safeMint(bob.address),
+                jackContract.safeMint(alice.address),
+                jackContract.safeMint(bob.address),
+                pmknToken.approve(lottery.address, ethers.utils.parseEther("25")),
+                lottery.addToLotteryPool(owner.address, ethers.utils.parseEther("25"))
+            ])
+        })
+
+        it("should call testGetRandomNumber", async() => {
+            await lottery.testGetWinningNumber()
+            expect(await lottery.lotteryCount())
+                .to.eq(1)
+            let reqId = await lottery.requestId()
+            expect(await lottery.requestIdToCount(reqId))
+                .to.eq(0)
+            expect(await lottery.winningNumber(0))
+                .to.eq(3)
+            expect(await jackContract.tokenOfOwnerByIndex(alice.address, 0))
+                .to.eq(0)
+        })
+
+        it("should payout bob", async() => {
+            let res = await pmknToken.balanceOf(lottery.address)
+            expect(res)    
+                .to.eq(ethers.utils.parseEther("25"))
+            await lottery.testGetWinningNumber()
+            await lottery.connect(bob).claimLottoWinnings()
+            expect(await pmknToken.balanceOf(lottery.address))
+                .to.eq(0)
+            expect(await pmknToken.balanceOf(bob.address))
+                .to.eq(ethers.utils.parseEther("25"))
+        })
+
+        it("should revert non-winner claims", async() => {
+            await lottery.testGetWinningNumber()
+            await expect(lottery.connect(owner).claimLottoWinnings())
+                .to.be.revertedWith("You either did not win or nothing in lotteryPool")
+            await expect(lottery.connect(alice).claimLottoWinnings())
+                .to.be.revertedWith("You either did not win or nothing in lotteryPool")
+        })
+    })
 })
-
-
-
-//const {use, expect} = require("chai");
-//const {ContractFactory, utils} = require("ethers");
-//const {MockProvider} = require("@ethereum-waffle/provider");
-//const {waffleChai} = require("@ethereum-waffle/chai");
-//const {deployMockContract} = require("@ethereum-waffle/mock-contract");
-//
-//const JackContract = require("../artifacts/contracts/JackOLantern.sol/JackOLantern.json");
-//const Lottery = require("../artifacts/contracts/Lottery.sol/Lottery.json");
-//const ChainlinkVRF = require("../artifacts/@chainlink/contracts/src/v0.8/dev/VRFConsumerBase.sol/VRFConsumerBase.json");
-//
-//use(waffleChai);
-//
-//describe("Lottery", () => {
-//  async function setup() {
-//    const [owner, alice, bob] = new MockProvider().getWallets();
-//    const mockVRF = await deployMockContract(owner, ChainlinkVRF.abi);
-//    const jackFactory = new ContractFactory(JackContract.abi, JackContract.bytecode, owner);
-//    const lotteryFactory = new ContractFactory(Lottery.abi, Lottery.bytecode, owner);
-//    const lotteryParams = [
-//        "0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9",
-//        "0xa36085F69e2889c224210F603D836748e7dC0088",
-//        1, 
-//        "0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4"    
-//    ]
-//    const jackContract = await jackFactory.deploy();
-//    const lottery = await lotteryFactory.deploy(jackContract.address, ...lotteryParams);
-//    return {owner, alice, bob, lottery, jackContract, mockVRF};
-//  }
-//
-//  // FIX ME
-//  it("should call VRF and store value in requestId", async () => {
-//    const {lottery, jackContract, mockVRF} = await setup();
-//    expect(await lottery.winningNumber(0))
-//        .to.eq(0)
-//    
-//    await mockVRF.mock.requestRandomness().returns(99)
-//    await lottery.getWinningNumber() // refactor tests, not working correctly
-//  });
-//
-//})
