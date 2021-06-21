8f99ca12479d9eeff6604557b1572290338b74b3
Andrew <fleming-andrew@protonmail.com>
2021-06-21 01:25:01 -0400

    Updated tests to ERC contracts

--- test/JackOLantern.test.ts
+++ test/JackOLantern.test.ts
@@ -1,5 +1,5 @@
 import { ethers } from "hardhat";
-import chai, { expect} from "chai";
+import chai, { expect } from "chai";
 import { Contract } from "ethers";
 import { solidity } from "ethereum-waffle";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
@@ -14,12 +14,12 @@ describe("JackOLantern Contract", () => {
 
     let jackOLantern: Contract;
 
-    before(async() => {
+    beforeEach(async() => {
         const JackOLantern = await ethers.getContractFactory("JackOLantern");
-
         [owner, alice, bob] = await ethers.getSigners();
-
         jackOLantern =  await JackOLantern.deploy()
+        let minter = await jackOLantern.MINTER_ROLE()
+        await jackOLantern.grantRole(minter, owner.address)
     })
 
     describe("Init", async() => {
@@ -30,7 +30,30 @@ describe("JackOLantern Contract", () => {
 
         it("has a name", async() => {
             expect(await jackOLantern.name())
-                .to.eq("Jack-O'-Lantern")
+                .to.eq("Jack-O-Lantern")
+        })
+
+        it("tracks tokens", async() => {
+            await jackOLantern.safeMint(owner.address)
+            await jackOLantern.safeMint(owner.address)
+
+            expect(await jackOLantern.getTotalSupply())
+                .to.eq(2)
+        })
+
+        it("should enumerate", async() => {
+            await jackOLantern.safeMint(owner.address)
+            await jackOLantern.safeMint(owner.address)
+            await jackOLantern.safeMint(owner.address)
+            await jackOLantern.safeMint(owner.address)
+            await jackOLantern.safeMint(owner.address)
+            await jackOLantern.transferFrom(owner.address, alice.address, 4)
+            let res = await jackOLantern.tokenOfOwnerByIndex(alice.address, 0)
+            expect(res).to.eq(4)
+            res = await jackOLantern.balanceOf(alice.address)
+            expect(res).to.eq(1)
+            res = await jackOLantern.balanceOf(owner.address)
+            expect(res).to.eq(4)
         })
     })
 })
